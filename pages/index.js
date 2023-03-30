9ca2de8e82217901ac3f1420e4c6f569c43e5c6a
alyshawang <113640728+alyshawang@users.noreply.github.com>
2023-03-30 00:12:30 -0400

    favicon

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,14 @@ import Landing from "@/components/Landing/Landing";
 import Projects from "@/components/Projects/Projects";
 import Contact from "@/components/Contact/Contact";
 import Footer from "@/components/Footer/Footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
    <>
+   <Head>
+    <link rel="shortcut icon" href="/Images/capyy.svg" />
+   </Head>
     <Landing />
     <Projects/>
     <Contact/>
